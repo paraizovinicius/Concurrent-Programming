1805ce36528dd949025317aa8307314831e911f7
paraizovinicius <95001119+paraizovinicius@users.noreply.github.com>
2023-12-05 17:11:51 +0100

    corrections

---
+++ Prova2022/PetitDejeuner.java
@@ -0,0 +1,98 @@
+package Prova2022;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Random;
+
+public class PetitDejeuner {
+  public static void main(String[] args) {
+    final int nbHobbits = 3;
+    final String nom[] = { "Bilbo", "Frodo", "Peregrin" };
+    Table table = new Table(nbHobbits);
+    for (int i = 0; i < nbHobbits; i++)
+      new Hobbit(nom[i], table).start();
+  }
+}
+
+class Table { // Les trois Hobbits mangent que quand tous les trois ont faim
+  final int nbHobbits;
+  private volatile int nbSentados = 0;
+
+  public Table(int nbHobbits) {
+    this.nbHobbits = nbHobbits;
+  }
+
+  public synchronized void sAsseoir() {
+    nbSentados++;
+
+    while (nbSentados != nbHobbits) { // a função só vai não-esperar quando nbSentados for igual a 0
+      try {
+        wait();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+    }
+    notifyAll();
+  }
+
+  public synchronized void seLever() {
+    nbSentados--;
+
+    while (nbSentados != 0) {
+      try {
+        wait();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    notifyAll();
+  }
+}
+
+class Hobbit extends Thread {
+  Table table;
+  Random aléa = new Random();
+
+  public Hobbit(String nom, Table table) {
+    this.setName(nom);
+    this.table = table;
+  }
+
+  private void affiche(String message) {
+    SimpleDateFormat sdf = new SimpleDateFormat("'['hh'h 'mm'mn 'ss','SSS's] '");
+    System.out.println(sdf.format(new Date(System.currentTimeMillis()))
+        + Thread.currentThread().getName() + " " + message);
+  }
+
+  private void petitDejeuner(String repas) {
+    affiche("commence son " + repas + " petit-déjeuner.");
+    try {
+      sleep(aléa.nextInt(5000));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    affiche("termine son " + repas + " petit-déjeuner.");
+  }
+
+  public void run() {
+    try {
+      sleep(aléa.nextInt(3000));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    affiche("est réveillé. Il a faim!");
+    table.sAsseoir();
+    petitDejeuner("1er");
+    table.seLever();
+    try {
+      sleep(aléa.nextInt(3000));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    affiche("a faim de nouveau!");
+    table.sAsseoir();
+    petitDejeuner("2nd");
+    table.seLever();
+  }
+}
